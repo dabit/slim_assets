9b6bf5a897ae3f57ce6551c57333769e18628ec9
David Padilla <david@michelada.io>
2017-01-25 13:51:09 -0800

    Update specs to new rspec syntax

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -5,18 +5,24 @@ end
 
 describe SlimAssets do
   it "should have the proper format" do
-    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should match /<h1><div class=\"text\">Render works<\/div><\/h1>/
+    expect(RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s).to match /<h1><div class=\"text\">Render works<\/div><\/h1>/
   end
 
   it "should use rails helpers" do
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /<form/
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /<input/
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /'<h1>Form<\/h1>/
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /id=&quot;post_title&quot;/
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /post\[title\]/
+    render = RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s
+    [
+      /<form/,
+      /<input/,
+      /'<h1>Form<\/h1>/,
+      /id=&quot;post_title&quot;/,
+      /post\[title\]/
+    ].each do |regexp|
+      expect(render).to match(regexp)
+    end
+
   end
 
   it "should use application helpers" do
-    RailsApp::Application.assets['app_helpers.jst.ejs.slim'].to_s.should match /<h1>App helpers work<\/h1>/
+    expect(RailsApp::Application.assets['app_helpers.jst.ejs.slim'].to_s).to match /<h1>App helpers work<\/h1>/
   end
 end

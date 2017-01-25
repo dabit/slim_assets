bea8ab2e6abe102e9cc3dcc1de43d5873c6c281f
David Padilla <david@michelada.io>
2017-01-25 11:57:25 -0800

    Fix the test suite and dependencies

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -5,11 +5,10 @@ end
 
 describe SlimAssets do
   it "should have the proper format" do
-    print RailsApp::Application.assets['link_to.jst.ejs.slim']
-    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should eq "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"link_to\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('h1\\n  .text Render works\\n\\n');}return __p.join('');};\n}).call(this);\n"
+    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should eq "(function() { this.JST || (this.JST = {}); this.JST[\"link_to\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1><div class=\"text\">Render works</div></h1>');}return __p.join('');};\n}).call(this);\n"
   end
 
   it "should use rails helpers" do
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('h1 Form\\n= form_for Post.new, url: \\'/\\' do |f|\\n  = f.text_field :title\\n');}return __p.join('');};\n}).call(this);\n"
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should == "(function() { this.JST || (this.JST = {}); this.JST[\"helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>Form</h1><form accept-charset=\"UTF-8\" action=\"/\" class=\"new_post\" id=\"new_post\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /></div>&lt;input id=&quot;post_title&quot; name=&quot;post[title]&quot; type=&quot;text&quot; /&gt;</form>');}return __p.join('');};\n}).call(this);\n"
   end
 end

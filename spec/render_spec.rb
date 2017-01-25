f53c3fc4700588737c9d7bc93087878cb469a138
David Padilla <david@michelada.io>
2017-01-25 13:01:03 -0800

    Make tests less brittle to different versions of Rails

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -5,10 +5,14 @@ end
 
 describe SlimAssets do
   it "should have the proper format" do
-    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should eq "(function() { this.JST || (this.JST = {}); this.JST[\"link_to\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1><div class=\"text\">Render works</div></h1>');}return __p.join('');};\n}).call(this);\n"
+    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should match "<h1><div class=\"text\">Render works</div></h1>"
   end
 
   it "should use rails helpers" do
-    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should == "(function() { this.JST || (this.JST = {}); this.JST[\"helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>Form</h1><form accept-charset=\"UTF-8\" action=\"/\" class=\"new_post\" id=\"new_post\" method=\"post\"><div style=\"margin:0;padding:0;display:inline\"><input name=\"utf8\" type=\"hidden\" value=\"&#x2713;\" /></div>&lt;input id=&quot;post_title&quot; name=&quot;post[title]&quot; type=&quot;text&quot; /&gt;</form>');}return __p.join('');};\n}).call(this);\n"
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /<form/
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /<input/
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /'<h1>Form<\/h1>/
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /id=&quot;post_title&quot;/
+    RailsApp::Application.assets['helpers.jst.ejs.slim'].to_s.should match /post\[title\]/
   end
 end

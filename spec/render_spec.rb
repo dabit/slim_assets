301a63ecabc7ce174b43e14f5e58616ba67bb761
David Padilla <david@michelada.io>
2017-01-25 13:18:19 -0800

    Change App helper test to the new format

--- spec/render_spec.rb
+++ spec/render_spec.rb
@@ -5,7 +5,7 @@ end
 
 describe SlimAssets do
   it "should have the proper format" do
-    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should match "<h1><div class=\"text\">Render works</div></h1>"
+    RailsApp::Application.assets['link_to.jst.ejs.slim'].to_s.should match /<h1><div class=\"text\">Render works</div></h1>/
   end
 
   it "should use rails helpers" do
@@ -17,6 +17,6 @@ describe SlimAssets do
   end
 
   it "should use application helpers" do
-    RailsApp::Application.assets['app_helpers.jst.ejs.slim'].to_s.should == "(function() {\n  this.JST || (this.JST = {});\n  this.JST[\"app_helpers\"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>App helpers work</h1>');}return __p.join('');};\n}).call(this);\n"
+    RailsApp::Application.assets['app_helpers.jst.ejs.slim'].to_s.should match /<h1>App helpers work<\/h1>/
   end
 end
